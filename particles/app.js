7aea6d37d37454baf7442ac382b66b706540c4ff
Andrii Burov <andriiburov@Andriis-MacBook-Pro.local>
2018-07-28 22:24:38 -0700

    add triangles

--- particles/app.js
+++ particles/app.js
@@ -152,3 +152,75 @@ particlesJS(
     retina_detect: true
   }
 );
+
+//SQUARES
+particlesJS(
+  "particles-js-squares",
+
+  {
+    particles: {
+      number: {
+        value: 8,
+        density: {
+          enable: true,
+          value_area: 1521.1926150101674
+        }
+      },
+      color: {
+        value: "#aaaa51"
+      },
+      shape: {
+        type: "triangle",
+        stroke: {
+          width: 0,
+          color: "#482d2d"
+        },
+        polygon: {
+          nb_sides: 2
+        },
+        image: {
+          src: "star.png",
+          width: 100,
+          height: 76
+        }
+      },
+      opacity: {
+        value: 0.7931929622151722,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 16,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+
+      move: {
+        enable: true,
+        speed: 1.6024100246771158,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 2323.494535781818,
+          rotateY: 1200
+        }
+      }
+    },
+
+    retina_detect: true
+  }
+);
